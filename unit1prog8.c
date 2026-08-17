b4d21f01f430cb9a720bc97f79c2c293f17da980
ouohijathnielira143821-eng <ouohijathnielira.143821@marwadiuniversity.ac.in>
2026-08-17 15:08:44 +0530

    Add files via upload

---
+++ unit1prog8.c
@@ -0,0 +1,31 @@
+/* 8. Sort the array into ascending order. */
+#include <stdio.h>
+
+int main() {
+    int arr[10], n = 10, i, j, temp;
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    /* bubble sort - ascending */
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("\nArray in ascending order: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
